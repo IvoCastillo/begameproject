41beb0e7b3290cd62fb76fae06d54b0d0ea3bee0
EddyGHendrickx <matthijs.hendrickx@gmail.com>
2020-04-02 15:19:21 +0200

    nee ivo

--- src/Controller/AdminOverviewController.php
+++ src/Controller/AdminOverviewController.php
@@ -13,21 +13,14 @@ class AdminOverviewController extends AbstractController
      */
     public function index()
     {
-        $questions = $this->getDoctrine()->getRepository(Question::class)->findAll();
 
         $allTeams = $this->getDoctrine()->getRepository(Team::class)->findAll();
 
 
         return $this->render('admin_overview/index.html.twig', [
             'controller_name' => 'AdminOverviewController',
-<<<<<<< HEAD
-            'questions' => $questions,
             'allTeams' => $allTeams,
-=======
-            'pendejos' => $allTeamPendejos,
->>>>>>> 5bf054106c573241038f015992ed6e13cb718f23
         ]);
-
 
     }
 }
