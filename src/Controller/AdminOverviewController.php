2bbc8e186cf04ef0a443a05439d36c89853cdf09
EddyGHendrickx <matthijs.hendrickx@gmail.com>
2020-04-02 15:06:59 +0200

    push before merge

--- src/Controller/AdminOverviewController.php
+++ src/Controller/AdminOverviewController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use App\Entity\Question;
+use App\Entity\Team;
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,11 +16,14 @@ class AdminOverviewController extends AbstractController
      */
     public function index()
     {
-        $questions = $this->getDoctrine()->getRepository(Question::class)->findAsArray();
+        $questions = $this->getDoctrine()->getRepository(Question::class)->findAll();
+
+        $allTeams = $this->getDoctrine()->getRepository(Team::class)->findAll();
 
         return $this->render('admin_overview/index.html.twig', [
             'controller_name' => 'AdminOverviewController',
             'questions' => $questions,
+            'allTeams' => $allTeams,
         ]);
     }
 }
